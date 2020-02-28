6cdf1767367bdaaa7f0ab2900d9e36ad5111be21
HmidaRojbani <hmida.rojbani@fst.utm.tn>
2020-02-28 11:24:10 +0100

    add my personal middleware

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const Joi = require('joi');
+const logger = require('./logger');
 const app = express();
 const port = 3000;
 app.use(express.json());
+app.use(logger);
 
 var students = [
     {id: 1, name: 'student1', age: 22},
