363cebf8596ea2b345c246a114424b3c6234307e
HmidaRojbani <hmida.rojbani@fst.utm.tn>
2020-02-28 10:45:35 +0100

    add Put & Delete

--- index.js
+++ index.js
@@ -47,4 +47,34 @@ app.post('/api/students', (req,res)=>{
     res.status(201).send(student);
 });
 
+app.delete('/api/students/:id', (req, res) => {
+    var res_validation = Joi.validate(req.params,student_id_valid_schema);
+    if(res_validation.error)
+        return res.status(400).send(res_validation.error.details[0].message);
+    const student_id = req.params.id;
+    const student = students.find(s => s.id === parseInt(student_id));
+    if(!student) 
+        return res.status(404).send(`Student with id : ${student_id} is not found`);
+    students = students.filter(s => s.id !== parseInt(student_id));
+    res.send(student);
+});
+
+app.put('/api/students/:id', (req, res) => {
+    var res_validation = Joi.validate(req.params,student_id_valid_schema);
+    if(res_validation.error)
+        return res.status(400).send(res_validation.error.details[0].message);
+    const student_id = req.params.id;
+    const student = students.find(s => s.id === parseInt(student_id));
+    if(!student) 
+        return res.status(404).send(`Student with id : ${student_id} is not found`);
+    res_validation = Joi.validate(req.body,student_valid_schema);
+        if(res_validation.error)
+            return res.status(400).send(res_validation.error.details[0].message);
+
+        student.name=  req.body.name;
+        student.age = req.body.age;
+    res.send(student);
+        
+});
+
 app.listen(port, ()=> console.log(`Listenning on ${port}....`))
